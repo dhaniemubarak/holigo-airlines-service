75f2ba789f92eb3d486d56937c2fc982f18addcf
Mochamad Ramdhanie Mubarak <dhaniemubarak@gmail.com>
2022-04-21 05:46:28 +0700

    Initial commit

---
+++ src/main/java/id/holigo/services/holigoairlinesservice/HoligoAirlinesServiceApplication.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigoairlinesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoligoAirlinesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoligoAirlinesServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/holigo/services/holigoairlinesservice/HoligoAirlinesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.holigo.services.holigoairlinesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoligoAirlinesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
